ff8c61a06593a87fd7a18aa679ac20899c7ad034
Enamul Haque <ehaque95@gmail.com>
2021-03-27 21:34:31 +0600

    first commit

---
+++ app/src/main/java/era/com/app/MainActivity.java
@@ -0,0 +1,14 @@
+package era.com.app;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
