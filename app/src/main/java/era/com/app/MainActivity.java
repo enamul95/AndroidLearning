d4f61da359d3c33e794c41d862d3b78c68c50a0a
Enamul Haque <ehaque95@gmail.com>
2021-03-31 22:59:54 +0600

    desing login pae & got second activity

--- app/src/main/java/era/com/app/MainActivity.java
+++ app/src/main/java/era/com/app/MainActivity.java
@@ -2,13 +2,33 @@ package era.com.app;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
+  Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        btnLogin = findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // got other activity or valiation
+                Intent intent = new Intent(MainActivity.this,Welcome.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
