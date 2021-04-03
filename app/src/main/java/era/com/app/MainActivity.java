c1af41d1b0adf575837b3f45dec87853688d9657
Enamul Haque <ehaque95@gmail.com>
2021-04-03 21:48:48 +0600

    pass value one page to another

--- app/src/main/java/era/com/app/MainActivity.java
+++ app/src/main/java/era/com/app/MainActivity.java
@@ -5,13 +5,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
-  Button btnLogin;
+
+    EditText etUsrName;
+    EditText etPassword;
+    Button btnLogin;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,14 +26,35 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
+        etUsrName = findViewById(R.id.etUsrName);
+        etPassword = findViewById(R.id.etPassword);
         btnLogin = findViewById(R.id.btnLogin);
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 // got other activity or valiation
-                Intent intent = new Intent(MainActivity.this,Welcome.class);
-                startActivity(intent);
+                //  Intent intent = new Intent(MainActivity.this,Welcome.class);
+                // startActivity(intent);
+                //  Log.e("Login clicke error ","successfullu pressed");
+                //Log.w("Login button waringin","successfullu pressed");
+
+                if(etUsrName.getText().toString().isEmpty()){
+                    Toast.makeText(getApplicationContext(),"Please Enter user name.",Toast.LENGTH_LONG).show();
+                }else if(etPassword.getText().toString().isEmpty()){
+                    Toast.makeText(MainActivity.this,"Please Enter password",Toast.LENGTH_LONG).show();
+                }else{
+                    Toast.makeText(MainActivity.this, "User name :"+etUsrName.getText().toString()+" Password :"+etPassword.getText().toString(), Toast.LENGTH_SHORT).show();
+
+                    Log.e("user name :",etUsrName.getText().toString());
+                    Log.e("Password :",etPassword.getText().toString());
+
+                     //got other activity or valiation
+                      Intent intent = new Intent(MainActivity.this,Welcome.class);
+                      intent.putExtra("user_name",etUsrName.getText().toString());
+                      startActivity(intent);
+                }
+
             }
         });
 
--- app/src/main/java/era/com/app/Welcome.java
+++ app/src/main/java/era/com/app/Welcome.java
@@ -2,13 +2,27 @@ package era.com.app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class Welcome extends AppCompatActivity {
+
+TextView lb_user_name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        lb_user_name = findViewById(R.id.lb_user_name);
+
+        Intent i = getIntent();
+        String username  = i.getStringExtra("user_name");
+
+        lb_user_name.setText(username);
+
+
     }
 }
